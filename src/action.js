d7bc429245dc5d61eaad25a78561b9099c1b7f2d
Javier LaRota <jlarota@barracuda.com>
2023-02-12 23:42:15 -0500

    test1

--- src/action.js
+++ src/action.js
@@ -1,24 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { spawn } = require('child_process');
+const cloc = require('node-cloc')
 
 async function run() {
-    const command = spawn('pwd');
-
-    command.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
-    });
-
-    command.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-    });   
-    command.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
-    });    
-    command.on('exit', function (code, signal) {
-        console.log('child process exited with ' +
-                    `code ${code} and signal ${signal}`);
-    });    
+    cloc('/my/folder').then((res) => console.log(res), (err) => console.log(err))
 }
 
 run();
